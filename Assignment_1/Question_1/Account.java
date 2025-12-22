2e61def334d182412f8f9f8b05f5bb7e9b24f95c
ThiQuangBA <165026426+DaiThiBA@users.noreply.github.com>
2025-12-22 14:09:10 +0700

    Assignment 2

--- Assignment_1/Question_1/Account.java
+++ Assignment_1/Question_1/Account.java
@@ -5,20 +5,20 @@ import java.time.LocalDate;
 public class Account {
     private int id;
     private String email;
-    private String username;
+    private String userName;
     private String fullName;
     private Department department;
     private Position position;
     private LocalDate createDate;
 
-    public Account (){
+    public Account() {
 
     }
 
     public Account(int id, String email, String username, String fullName, Department department, Position position, LocalDate createDate) {
         this.id = id;
         this.email = email;
-        this.username = username;
+        this.userName = username;
         this.fullName = fullName;
         this.department = department;
         this.position = position;
@@ -42,12 +42,12 @@ public class Account {
         this.email = email;
     }
 
-    public String getUsername() {
-        return username;
+    public String getUserName() {
+        return userName;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setUsername(String userName) {
+        this.userName = userName;
     }
 
     public String getFullName() {
@@ -81,4 +81,36 @@ public class Account {
     public void setCreateDate(LocalDate createDate) {
         this.createDate = createDate;
     }
+
+//    @Override
+//    public String toString() {
+//        return "Thông tin account thứ " + id + " là:\n"
+//                + "Email: " + email + "\n"
+//                + "Full name: " + fullName + "\n"
+//                + "Phòng ban: " + department;
+//    }
+
+//    @Override
+//    public boolean equals(Object obj) {
+//        // 1. So sánh cùng địa chỉ
+//        if (this == obj) {
+//            return true;
+//        }
+//
+//        // 2. Kiểm tra null
+//        if(obj == null) {
+//            return false;
+//        }
+//
+//        // 3. Kiểm tra cùng class
+//        if( getClass() != obj.getClass()){
+//            return false;
+//        }
+//        // 4. Ép kiểu để gọi được thuộc tính
+//        Account other = (Account) obj;
+//
+//        // 5. So sánh thuộc tính
+//        return id == other.getId();
+//
+//    }
 }
